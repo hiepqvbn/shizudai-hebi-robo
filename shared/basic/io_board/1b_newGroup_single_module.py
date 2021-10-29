6120e4b25f24adf6ed13ad08d2b0687b0c6ec927
Ken <Ken@email.com>
2021-10-29 13:15:43 +0900

    retry to push basic

---
+++ shared/basic/io_board/1b_newGroup_single_module.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import hebi
+from time import sleep
+
+lookup = hebi.Lookup()
+
+# Wait 2 seconds for the module list to populate
+sleep(2.0)
+
+family_name = "HEBI"
+module_name = "IO Board"
+
+group = lookup.get_group_from_names([family_name], [module_name])
+
+if group is None:
+  print('Group not found: Did you forget to set the module family and name above?')
+  exit(1)
+
+print('Created group from module {0} | {1}.'.format(family_name, module_name))
---
+++ shared/basic/io_board/1c_newGroup_multiple_modules.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import hebi
+from time import sleep
+
+lookup = hebi.Lookup()
+
+# Wait 2 seconds for the module list to populate
+sleep(2.0)
+
+family_name = "HEBI"
+module_names = ["IO Board", "mobileIO", "Actuator 1"]
+
+group = lookup.get_group_from_names([family_name], module_names)
+
+if group is None:
+  print('Group not found: Did you forget to set the module family and name above?')
+  exit(1)
+
+print('Found group on network with {0} modules.'.format(group.size))
